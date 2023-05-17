0343aa00e60febac692e83e8ad552d747fb4ddf0
0Gimenes <gabrielgimenes9353@gmail.com>
2023-05-17 17:34:05 -0400

    mudei mostrar produtos

--- script.js
+++ script.js
@@ -8,17 +8,30 @@ class Produto {
             this.preco = preco
     }
 mostrap(){
-    this.nome + this.datac + this.descricao + this.preco
+   return`
+   <h1>${this.nome}</h1>
+   <h2>${this.descricao}</h2>
+   `
 }
 }
 class Produtod extends Produto{
-    constructor (nome , datac,descricao , preco, imagemd){
+    constructor (nome , datac,descricao , preco, imagem){
         super(nome , datac , descricao, preco )
-this.imagemd = imagemd
-        
+this.imagem = imagem
     }
 
 mostrapd(){
- return this.imagemd 
+ return` <img src="${this.imagem}">
+ <h1>${this.nome}</h1>
+ <h2>${this.descricao}</h2>`
 }
 }
+
+const produto = new Produto("Rei chikita", "24/5/2021","Pelucia de anime", 2.33,"https://i.pinimg.com/originals/03/9d/7c/039d7ce116e639b24efbc9b66475fd44.jpg")
+console.log(produto.mostrap())
+
+const produtopd = document.getElementById("lista-produtos")
+produtopd.insertAdjacentHTML('afterbegin', produto.mostrap())
+
+const produtod = document.getElementById("lista-produtos")
+produtopd.insertAdjacentHTML('afterbegin', produto.mostrapd())
