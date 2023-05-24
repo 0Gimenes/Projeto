15355e08cc56742eb4cd8060c55ee32bd69b0c38
0Gimenes <gabrielgimenes9353@gmail.com>
2023-05-24 17:36:40 -0400

    mudei o Projeto

--- script.js
+++ script.js
@@ -7,28 +7,36 @@ class Produto {
             this.descricao = descricao;
             this.preco = preco
     }
+
+
 mostrap(){
-   return`
-   <h1>${this.nome}</h1>
-   <h2>${this.descricao}</h2>
-   `
+   return this.nome + this.datac + this.descricao + this.preco
 }
 }
+
+const produto = new Produto("Rei chikita", "24/5/2021","Pelucia de anime", 2.33,"https://i.pinimg.com/originals/03/9d/7c/039d7ce116e639b24efbc9b66475fd44.jpg")
+console.log(produto.mostrap())
+
 class Produtod extends Produto{
     constructor (nome , datac,descricao , preco, imagem){
-        super(nome , datac , descricao, preco )
+        super(nome , datac , descricao, preco , imagem)
 this.imagem = imagem
     }
 
 mostrapd(){
  return` <img src="${this.imagem}">
  <h1>${this.nome}</h1>
- <h2>${this.descricao}</h2>`
+ <h2>${this.descricao}</h2>
+ <h2>${this.datac}</h2>
+ <h2>${this.preco}</h2>
+ 
+ 
+ `
 }
 }
 
-const produto = new Produto("Rei chikita", "24/5/2021","Pelucia de anime", 2.33,"https://i.pinimg.com/originals/03/9d/7c/039d7ce116e639b24efbc9b66475fd44.jpg")
-console.log(produto.mostrap())
+const Mostrapd = new mostrapd("Rei chikita", "24/5/2021","Pelucia de anime", 2.33,"https://i.pinimg.com/originals/03/9d/7c/039d7ce116e639b24efbc9b66475fd44.jpg")
+console.log(Mostrapd.mostrapd())
 
 const produtopd = document.getElementById("lista-produtos")
 produtopd.insertAdjacentHTML('afterbegin', produto.mostrap())
