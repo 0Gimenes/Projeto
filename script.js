9a905cb51d3964d92c228b6d3fa75341bc03d702
0Gimenes <gabrielgimenes9353@gmail.com>
2023-06-14 17:43:19 -0400

    adicionei os erros

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // divListaProduto.insertAdjacentHTML('afterbegin', '');
+class myErro extends Error {
+    constructor(message){
+      super(message);
+      this.name = "myErro";
+    }
+  }
+
 
 class Produto {
     constructor(nome, datac, descricao, preco){
@@ -8,38 +15,69 @@ class Produto {
             this.preco = preco
     }
 
-
+    mostrarAtributos(){
+        try {
+          return this.mostrap();  
+        } catch (error) {
+          console.log(error) 
+        }
+      } 
+    
 mostrap(){
-   return this.nome + this.datac + this.descricao + this.preco
-}
-}
-
-const produto = new Produto("Rei chikita", "24/5/2021","Pelucia de anime", 2.33,"https://i.pinimg.com/originals/03/9d/7c/039d7ce116e639b24efbc9b66475fd44.jpg")
-console.log(produto.mostrap())
-
-class Produtod extends Produto{
-    constructor (nome , datac,descricao , preco, imagem){
-        super(nome , datac , descricao, preco , imagem)
-this.imagem = imagem
-    }
-
-mostrapd(){
- return` <img src="${this.imagem}">
+ if(this.nome != "" && this.descricao != "" && this.datac !="" && this.preco !=""){  return`
  <h1>${this.nome}</h1>
  <h2>${this.descricao}</h2>
  <h2>${this.datac}</h2>
  <h2>${this.preco}</h2>
- 
- 
- `
+`
+} else{
+    throw new myErro("Deu erro em algum campo dos produtos")
 }
 }
+}
 
-const Mostrapd = new mostrapd("Rei chikita", "24/5/2021","Pelucia de anime", 2.33,"https://i.pinimg.com/originals/03/9d/7c/039d7ce116e639b24efbc9b66475fd44.jpg")
-console.log(Mostrapd.mostrapd())
 
-const produtopd = document.getElementById("lista-produtos")
-produtopd.insertAdjacentHTML('afterbegin', produto.mostrap())
 
-const produtod = document.getElementById("lista-produtos")
-produtopd.insertAdjacentHTML('afterbegin', produto.mostrapd())
+class Produtod extends Produto{
+    constructor (nome , datac, descricao , preco, imagem){
+        super(nome , datac , descricao, preco , imagem)
+this.imagem = imagem
+    }
+
+    mostrarAtributosd(){
+        try {
+          return this.mostrapd();  
+        } catch (error) {
+          console.log(error) 
+        }
+      } 
+
+    mostrapd(){
+        if(this.nome != "" && this.descricao != "" && this.datac !="" && this.preco !="" && this.imagem !="" ){
+            return` <img src="${this.imagem}">
+            <h1>${this.nome}</h1>
+            <h2>${this.descricao}</h2>
+            <h2>${this.datac}</h2>
+            <h2>${this.preco}</h2>`
+        }
+        else{
+            throw new myErro("Deu erro em algum campo dos destaques")
+        }
+    }
+}
+
+const rei = new Produtod ("Rei chikita", "24/5/2021","Pelucia de anime", 2.33,"https://i.pinimg.com/originals/03/9d/7c/039d7ce116e639b24efbc9b66475fd44.jpg")
+const cow = new Produto ("vaca", "24/5/2021","vaquinha cabulosa", )
+const cat = new Produto ("gato", "24/5/2021","gato colossal", 15.33)
+
+
+
+const produtod = document.getElementById("produto-destaque");
+produtod.insertAdjacentHTML('afterbegin', rei.mostrapd());
+
+
+const produtoc = document.getElementById("lista-produtos")
+produtoc.insertAdjacentHTML('afterbegin', cow.mostrap())
+
+const produtoco = document.getElementById("lista-produtos")
+produtoco.insertAdjacentHTML('afterbegin', cat.mostrap())
